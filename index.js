7dbf666be6813724038d192e5aba0486eae89a26
Johnny Wu <KarasAya@gmail.com>
2017-02-03 10:29:53 +0800

    update

---
+++ index.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const path_ = require('path');
+let pathPlus = {};
+
+/**
+ * @method basenameNoExt
+ * @param {string} path
+ */
+pathPlus.basenameNoExt = function ( path ) {
+  return path_.basename(path, path_.extname(path) );
+};
+
+/**
+ * @method slash
+ * @param {string} path
+ */
+pathPlus.slash = function ( path ) {
+  // var isExtendedLengthPath = /^\\\\\?\\/.test(path);
+  // var hasNonAscii = /[^\x00-\x80]+/.test(path);
+  // if (isExtendedLengthPath || hasNonAscii) {
+  //     return path;
+  // }
+  return path.replace(/\\/g, '/');
+};
+
+/**
+ * @method stripSep
+ * @param {string} path
+ */
+pathPlus.stripSep = function ( path ) {
+  path = path_.normalize(path);
+  for ( var i = path.length-1; i >= 0; --i ) {
+    if ( path[i] !== path_.sep ) {
+      break;
+    }
+  }
+  return path.substring(0,i+1);
+};
+
+/**
+ * @method stripExt
+ * @param {string} path
+ */
+pathPlus.stripExt = function ( path ) {
+  var extname = path_.extname(path);
+  return path.substring(0, path.length-extname.length);
+};
+
+/**
+ * @method contains
+ * @param {string} pathA
+ * @param {string} pathB
+ *
+ * Examples:
+ *
+ *   pathA = foo/bar,         pathB = foo/bar/foobar, return true
+ *   pathA = foo/bar,         pathB = foo/bar,        return true
+ *   pathA = foo/bar/foobar,  pathB = foo/bar,        return false
+ *   pathA = foo/bar/foobar,  pathB = foobar/bar/foo, return false
+ */
+pathPlus.contains = function ( pathA, pathB ) {
+  pathA = pathPlus.stripSep(pathA);
+  pathB = pathPlus.stripSep(pathB);
+
+  if ( process.platform === 'win32' ) {
+    pathA = pathA.toLowerCase();
+    pathB = pathB.toLowerCase();
+  }
+
+  //
+  if ( pathA === pathB ) {
+    return true;
+  }
+
+  // never compare files
+  if ( path_.dirname(pathA) === path_.dirname(pathB) ) {
+    return false;
+  }
+
+  if ( pathA.length < pathB.length &&
+      pathB.indexOf (pathA + path_.sep) === 0 ) {
+        return true;
+      }
+
+      return false;
+};
+
+// ========================================
+// exports
+// ========================================
+
+let _ = {};
+for ( let p in path_ ) {
+  _[p] = path_[p];
+}
+for ( let p in pathPlus ) {
+  _[p] = pathPlus[p];
+}
+
+module.exports = _;
---
+++ test/basic.spec.js
@@ -0,0 +1,134 @@
+'use strict';
+
+const tap = require('tap');
+const pathPlus = require('../index');
+
+tap.test('pathPlus.contains', t => {
+  // should work for simple case 
+  t.ok(
+    pathPlus.contains('foo/bar', 'foo/bar/foobar'),
+    'foo/bar > foo/bar/foobar'
+  );
+
+  t.ok(
+    pathPlus.contains('foo/bar', 'foo/bar'),
+    'foo/bar == foo/bar'
+  );
+
+  t.notOk(
+    pathPlus.contains('foo/bar/foo-01', 'foo/bar/foo-02'),
+    'foo/bar/foo-01 != foo/bar/foo-02'
+  );
+
+  t.notOk(
+    pathPlus.contains('foo/bar/foobar', 'foo/bar'),
+    'foo/bar/foobar < foo/bar'
+  );
+
+  t.notOk(
+    pathPlus.contains('foo/bar/foobar', 'foobar/bar/foo'),
+    'foo/bar/foobar != foobar/bar/foo'
+  );
+
+  t.notOk(
+    pathPlus.contains('foo/bar/foobar/foobar.js', 'foo/bar/foobar/foobar.js.meta'),
+    'foo/bar/foobar/foobar.js != foo/bar/foobar/foobar.js.meta'
+  );
+
+  t.ok(
+    pathPlus.contains('foo/bar/foobar', 'foo/bar/foobar/foobar.js'),
+    'foo/bar/foobar > foo/bar/foobar/foobar.js'
+  );
+
+  t.notOk(
+    pathPlus.contains('foo/bar', 'foo/bar1/foobar'),
+    'foo/bar != foo/bar1/foobar'
+  );
+
+  t.ok(
+    pathPlus.contains('foo/bar1/', 'foo/bar1/foobar'),
+    'foo/bar1/ == foo/bar1/foobar'
+  );
+
+  // should work for complex typing
+  t.ok(
+    pathPlus.contains('foo/bar/', 'foo/bar/foobar'),
+    'foo/bar/ > foo/bar/foobar'
+  );
+
+  t.ok(
+    pathPlus.contains('foo/bar', 'foo/bar/'),
+    'foo/bar == foo/bar/'
+  );
+
+  t.notOk(
+    pathPlus.contains('foo/bar/foo-01/', 'foo/bar/foo-02'),
+    'foo/bar/foo-01/ != foo/bar/foo-02'
+  );
+
+  // should work for case insensitive system
+  t.equal(
+    pathPlus.contains('c:\\foo\\bar', 'C:\\Foo\\Bar'),
+    process.platform === 'win32',
+    'c:\\foo\\bar === C:\\Foo\\Bar in Windows'
+  );
+
+  t.end();
+});
+
+tap.test('pathPlus.slash', t => {
+  // should convert backwards-slash paths to forward slash paths
+  t.equal(
+    pathPlus.slash('c:/aaaa\\bbbb'),
+    'c:/aaaa/bbbb'
+  );
+
+  t.equal(
+    pathPlus.slash('c:\\aaaa\\bbbb'),
+    'c:/aaaa/bbbb'
+  );
+
+  // t.test('should not convert extended-length paths', function () {
+  //     var path = '\\\\?\\c:\\aaaa\\bbbb';
+  //     pathPlus.slash(path)
+  //     .should.eql(path);
+  // });
+
+  // should convert paths with Unicode
+  t.equal(
+    pathPlus.slash('c:\\aaaa\\bbbb\\★'),
+    'c:/aaaa/bbbb/★'
+  );
+
+  // should convert paths with Chinese
+  t.equal(
+    pathPlus.slash('c:\\aaaa\\bbbb\\你好'),
+    'c:/aaaa/bbbb/你好'
+  );
+
+  t.end();
+});
+
+tap.test('pathPlus.stripExt', t => {
+  t.equal(
+    pathPlus.stripExt('foo/bar/foobar.sprite'),
+    'foo/bar/foobar'
+  );
+
+  t.equal(
+    pathPlus.stripExt('foo/bar/foobar'),
+    'foo/bar/foobar'
+  );
+
+  t.equal(
+    pathPlus.stripExt('foo/bar/foobar.sprite.foo'),
+    'foo/bar/foobar.sprite'
+  );
+
+  t.equal(
+    pathPlus.stripExt('foo/bar/foobar.png/hello.sprite'),
+    'foo/bar/foobar.png/hello'
+  );
+
+  t.end();
+});
